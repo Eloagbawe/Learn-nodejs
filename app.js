ece877ed58e2275e20049501e57acd026aa37f8d
Eloagbawe <eloagbawe@gmail.com>
2020-05-14 23:34:05 +0100

    still on modules, exports and require

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var fullName = require("./modules-exports-require.js");
+var deets = require("./require-exports");
+var subjects = require("./require.js");
 
 fullName();
+
+subjects.subject1();
+
+console.log(deets.name);
