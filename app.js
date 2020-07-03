0f1bffce82faa9df93a03701a47f6f2624e7d78b
Eloagbawe <eloagbawe@gmail.com>
2020-07-03 12:58:24 +0100

    http

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var deets = require("./require-exports");
 var subjects = require("./require.js");
 var greet = require("./function_constructor.js");
 var greet2 = require("./hello.js").greet;
+const http = require("http");
 
 fullName();
 
@@ -11,3 +12,9 @@ subjects.subject1();
 console.log(deets.name);
 greet.greet();
 greet2();
+
+http.createServer(function(req,res){
+    res.writeHead(200, {"content-type": "application/json"});
+    res.end("Hello World");
+    
+}).listen(4000, '127.0.0.1');
